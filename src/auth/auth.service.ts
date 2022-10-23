026ff738555effa3d94bcefa1963b30d518bb66d
spaceofmiah <geek_200@outlook.com>
2022-10-23 20:06:20 +0100

    Inject PrismaService as dependency

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import {Injectable} from '@nestjs/common';
+import {PrismaService} from '../prisma/prisma.service';
 
-
-@Injectable({})
+@Injectable()
 export class AuthService {
+
+	constructor(private prisma:PrismaService) {}
 
 	signup(){
 		return {
