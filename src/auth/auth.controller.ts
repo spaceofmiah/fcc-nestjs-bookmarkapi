eddd194146a5fb3786ffc4035efd44819747ab80
spaceofmiah <geek_200@outlook.com>
2022-10-23 13:42:20 +0100

    Add basic route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,18 @@
-import {Controller} from '@nestjs/common'
+import {Controller, Post} from '@nestjs/common';
+import {AuthService} from './auth.service';
 
 
-@Controller({})
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+	constructor(private authService: AuthService){}
+
+	@Post("/signin")
+	signin() {
+		return 'Login Successful';
+	}
+
+	@Post("/signup")
+	signup() {
+		return 'Registration Successful';
+	}
+}
