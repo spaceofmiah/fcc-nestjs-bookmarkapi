3fcb7c393ae16bbfc1059d2ff4b1275593f8dda9
spaceofmiah <geek_200@outlook.com>
2022-10-23 13:32:25 +0100

    Create skeletal Controller & Service class

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,11 @@
 import {Module} from '@nestjs/common';
+import {AuthService} from './auth.service';
+import {AuthController} from './auth.controller';
 
-@Module({})
+@Module({
+	controllers: [AuthController],
+	providers: [AuthService]
+})
 export class AuthModule {
 
 }
