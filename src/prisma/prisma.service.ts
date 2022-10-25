6b9a5b895aaab947b249d8c2e3e3bebabc1f65de
spaceofmiah <geek_200@outlook.com>
2022-10-25 12:01:44 +0100

    Read configuration variables from .env file

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import {PrismaClient} from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-	constructor() {
+	constructor(config: ConfigService) {
 		super({
 			datasources: {
 				db: {
-					url: "postgresql://admin:password@localhost:5432/fccbookmarkapi?schema=public"
+					url: config.get('DATABASE_URL')
 				}
 			}
 		})
