048a772f7021e9ad503f4263c507ed4084b2e0a2
spaceofmiah <geek_200@outlook.com>
2022-10-26 22:49:42 +0100

    Replace @Req param decorator with @GetUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { JwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
 
-	@UseGuards(JwtGuard)
 	@Get("me")
-	getMe(@Req() req: Request) {
-		return req.user
+	getMe(@GetUser() user: User) {
+		return user
 	}
 }
