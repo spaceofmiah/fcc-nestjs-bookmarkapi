4e1928aa23a279175014ff84f892a9bbdd92b671
spaceofmiah <geek_200@outlook.com>
2022-10-26 22:27:21 +0100

    Retrieve user from db using validated token payload

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -3,19 +3,32 @@ import {PassportStrategy} from '@nestjs/passport';
 import {ConfigService} from '@nestjs/config';
 import {Injectable} from '@nestjs/common';
 
+import {PrismaService} from '../../prisma/prisma.service';
+
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy (
 	Strategy, 'jwt' 
 ) {
-	constructor(private config:ConfigService) {
+	constructor(
+		private config:ConfigService,
+		private prismaService:PrismaService
+	) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			secretOrKey: config.get('JWT_SECRET'),
 		})
 	}
 
-	validate(payload) {
-		return payload
+	async validate(payload:{sub:number, email:string}) {
+		const user =  await this.prismaService.user.findUnique({
+			where: {
+				id: payload.sub
+			}
+		});
+
+		delete user.hash;
+		return user;
 	}
 }
