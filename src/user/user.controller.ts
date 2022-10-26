55f2fb2af08fb90bc1a7d6d182208b4ee3cd3b3f
spaceofmiah <geek_200@outlook.com>
2022-10-26 22:28:36 +0100

    Utilize JwtGuard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport'
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from '../auth/guard';
 
 @Controller('users')
 export class UserController {
 
-	@UseGuards(AuthGuard('jwt'))
+	@UseGuards(JwtGuard)
 	@Get("me")
-	getUser() {
-		return 'user info'
+	getMe(@Req() req: Request) {
+		return req.user
 	}
 }
