8987f9bbc1ea6cdcc9a9d3c19c7dac813c75a1a1
spaceofmiah <geek_200@outlook.com>
2022-10-27 07:33:52 +0100

    Include startup and teardown of resource for testcases

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,38 @@
+import {INestApplication, ValidationPipe, } from '@nestjs/common';
 import {Test} from '@nestjs/testing';
+
 import {AppModule} from '../src/app.module'
 
 describe('App e2e', () => {
+  let app:INestApplication;
+  // Make replica of the main app run by the dev server
+  // but solely for testing
+  
+  // Startup :: will be called when test is completed to remove
+  // resource used by the test 
   beforeAll(async() => {
+
+
+    // Load the application module
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+
+    // create a nest application with the loaded modules
+    app = moduleRef.createNestApplication(); 
+
+    // utilize globals
+    app.useGlobalPipes(new ValidationPipe({whitelist: true}));
+
+    // initialize the app
+    await app.init()
   });
+
+  // Breakdown :: will be called when test is completed to remove
+  // resource used by the test 
+  afterAll(async() => {
+    app.close();
+  })
 
   it.todo('should pass');
 })
