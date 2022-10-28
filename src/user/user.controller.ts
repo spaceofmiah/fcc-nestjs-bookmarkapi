d25b3bc67ac97727d3aff4582de522e7afbf11bc
spaceofmiah <geek_200@outlook.com>
2022-10-28 04:49:45 +0100

    Handle patch request for user edit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { 
+	Controller, 
+	Get, 
+	UseGuards,
+	Body,
+	Patch,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
 import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
+import { UserService } from './user.service';
+import { EditUserDto } from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+	constructor(private userService:UserService){}
 
 	@Get("me")
 	getMe(@GetUser() user: User) {
 		return user
+	}
+
+	@Patch()
+	editUser(
+		@GetUser('id') userId: number, 
+		@Body() dto: EditUserDto
+	) {
+		return this.userService.editUser(userId, dto)
 	}
 }
